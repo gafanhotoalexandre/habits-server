3d8f6c7acf2198e968c162af891cc5b60077285d
Alexandre Vitorino Martins de Lima <alexandrevmartinsdelima@gmail.com>
2023-04-16 22:33:38 -0300

    criando rota toggle para habits

--- src/routes.ts
+++ src/routes.ts
@@ -73,12 +73,69 @@ export async function appRoutes(app: FastifyInstance) {
 
 		const completedHabits = day?.dayHabits.map(dayHabit => {
 			return dayHabit.habit_id;
-		});
+		}) ?? [];
 
 		return {
 			possibleHabits,
 			completedHabits
 		}
-	})
+	});
+
+	// toggle para completar hábito
+	app.patch('/habits/:id/toggle', async (request) => {
+		const toggleHabitsParams = z.object({
+			id: z.string().uuid(),
+		});
+
+		const { id } = toggleHabitsParams.parse(request.params);
+
+		const today = dayjs().startOf('day').toDate();
+
+		let day = await prisma.day.findUnique({
+			where: {
+				date: today
+			}
+		});
+
+		if (!day) {
+			day = await prisma.day.create({
+				data: {
+					date: today
+				},
+			});
+		}
+
+		const dayHabit = await prisma.dayHabit.findUnique({
+			where: {
+				day_id_habit_id: {
+					day_id: day.id,
+					habit_id: id
+				}
+			}
+		});
+
+		if (dayHabit) {
+			// remover a marcação de hábito completo
+			await prisma.dayHabit.delete({
+				where: {
+					id: dayHabit.id
+				}
+			});
+			return {
+				message: 'Deletado com sucesso'
+			}
+		} else {
+			// completar o hábito no dia
+			await prisma.dayHabit.create({
+				data: {
+					day_id: day.id,
+					habit_id: id
+				}
+			});
+			return {
+				message: 'Criado com sucesso'
+			}
+		}
+	});
 
 }
